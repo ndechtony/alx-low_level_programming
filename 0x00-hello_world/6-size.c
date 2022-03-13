cbb2167e0fbf445ee73c3e576ace26959994ba7e
ndechtony <ndegwaa3@gmail.com>
2022-03-13 09:13:56 -0700

    file 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(“TypettttSize (bytes)”);
+	printf(“nCharacterttt    %d”,sizeof(char));
+	printf(“nIntegertttt    %d”,sizeof(int));
+	printf(“nLong intttt    %d”,sizeof(long int));
+	printf(“nFloattttt    %d”,sizeof(float));
+	printf(“nDoubletttt    %d”,sizeof(double));
+	printf(“nLong doublettt    %d”,sizeof(long double));
+        return (0);
+}
