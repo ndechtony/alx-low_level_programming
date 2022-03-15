ca10759de0874212fb42e45d66d4ac60e73d1cc8
ndechtony <ndegwaa3@gmail.com>
2022-03-15 14:20:24 -0700

    file 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,36 +1,22 @@
 #include "main.h"
 /**
- * times_table - function that prints the 9 times table, starting with 0
- * @void: no argument
+ * jack_bauer - prints every minute of the day 00:00 to 23:59
+ * @void - no argument
  */
-void times_table(void)
+void jack_bauer(void)
 {
-	int i, j, k;
+	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 60; j++)
 		{
-			k = i * j;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
-			else if (k <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
-			else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
