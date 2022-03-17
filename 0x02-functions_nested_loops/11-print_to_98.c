103ac281c4c1143c238d2b65976cf5dcf1762788
ndechtony <ndegwaa3@gmail.com>
2022-03-17 13:18:45 -0700

    file 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,49 +1,33 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- i* print_times_table - prints the times table
- * @n: takes in an integer
+ * print_to_98 - Print all natural numbers from n to 98
+ * @n: int type number
  */
-void print_times_table(int n)
+void print_to_98(int n)
 {
-	int i, j, k;
-
-	if (n > 15 || n < 0)
-		return;
-	for (i = 0; i <= n; i++)
+	if (n > 98)
 	{
-		for (j = 0; j <= n; j++)
+		while (n >= 98)
 		{
-			k = i * j;
-			if (j == 0)
+			printf("%d", n);
+			if (n != 98)
 			{
-				_putchar(k + '0');
+				printf(", ");
 			}
-			else if (k <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
-			else if (k >= 10 && k < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(k / 10 % 10 + '0');
-				_putchar(k % 10 + '0');
-			}
+			n--;
 		}
-		_putchar('\n');
 	}
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	printf("\n");
 }
